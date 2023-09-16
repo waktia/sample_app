2ce4746ea2fe11e7734f4cdd4c86dd2d329ef67a
waktia <wakita.904@gmail.com>
2023-09-16 11:47:16 +0000

    change bin/render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-#exit on error
+# exit on error
 set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec db:migrate
+bundle exec rails db:migrate
 
