0f336b988181575d1a15ad69b9c70588c3134968
waktia <wakita.904@gmail.com>
2023-09-30 14:20:08 +0000

    Add Follow

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,17 @@ Rails.application.routes.draw do
   get "/login",     to: "sessions#new"
   post "/login",    to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
