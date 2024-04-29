7a1fea43590ab6a1984cf896319b7528eaad46ce
Kohei Wakita <wakita.904@gmail.com>
2024-04-29 11:04:26 +0900

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
  root 'static_pages#home'
 
   get    '/help',    to: 'static_pages#help'
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
